b30b8884d2bdc3d5960d5fdc8454b7475bcd7d88
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-09-03 14:35:29 -0500

    style data in cards and center items

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="home">
     <div v-for="coach in coaches">
-      <p> {{coach.name}} </p>
-      <div v-for="time_slot in coach.time_slots">
-        <p>{{time_slot.day}}:{{time_slot.time_slot}}-{{time_slot.status}}</p>
-        <p>{{time_slot.timezone}}</p>
+      <div class="container">
+        <div class="card">
+          <div class="card-header">
+            <h3>{{coach.name}} </h3>
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Choose your appointment</h5>
+            <p class="card-text">
+              <div class="row">
+                <div class="col-4" v-for="time_slot in coach.time_slots">
+                  <p class="align-center">{{time_slot.day}}:{{time_slot.time_slot}}-{{time_slot.status}}</p>
+                  <p class="timezone-text">{{time_slot.timezone}}</p>
+                  <p class="align-center"><a href="#" class="btn btn-primary">Go somewhere</a></p>
+                </div>
+              </div>
+            </p>
+          </div>
+        </div>
+        <p></p>
+        
       </div>
       <hr>
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.timezone-text{
+  font-size: 12px;
+  color: #747474;
+  text-align: center;
+}
+.align-center{
+  text-align: center;
+}
+</style>
 
 <script>
   import axios from "axios"
