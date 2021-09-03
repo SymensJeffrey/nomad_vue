27ae8e73295979ab115c897135a88c35d9296d4f
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-09-03 15:45:12 -0500

    change title on navbar

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav class="navbar navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand">Navbar</a>
+        <a class="navbar-brand">Coach Appointments</a>
       </div>
     </nav>
     <router-view/>
